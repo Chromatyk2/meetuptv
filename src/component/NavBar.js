9dca576fa076d2b0b87c118f0118e78c4a93a0a5
pierre schuvey <chromatykretro@gmail.com>
2024-09-18 16:23:31 +0200

    fix

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,16 +1,18 @@
 import React,{useState} from 'react';
 
 function NavBar() {
-    const list = document.querySelectorAll(".list");
+    <script>
+        const list = document.querySelectorAll(".list");
 
-    function activeLink() {
+        function activeLink() {
         list.forEach((item) => item.classList.remove('active'));
         this.classList.add('active');
     };
 
-    list.forEach((item) => {
+        list.forEach((item) => {
         item.addEventListener('click', activeLink)
     });
+    </script>
     return (
         <div className="navbar">
             <ul>
