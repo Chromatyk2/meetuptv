d7d4481dc32e2ea4e453bfa303ab7e2561fd0385
pierre schuvey <chromatykretro@gmail.com>
2024-09-18 16:21:34 +0200

    fix

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,25 +1,13 @@
-import React,{useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom'
-import Container from 'react-bootstrap/Container';
-import Axios from 'axios'
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import {BrowserRouter, Link} from "react-router-dom";
-import env from "react-dotenv";
+import React,{useState} from 'react';
 
-function NavBar(props) {
-    const [count, setCount] = useState(0);
-    const [stream, setStream] = useState(null);
-    const pseudo = props.cookies.user.data[0].login;
-    const [expanded, setExpanded] = useState(false);
+function NavBar() {
     const list = document.querySelectorAll(".list");
 
     function activeLink() {
         list.forEach((item) => item.classList.remove('active'));
         this.classList.add('active');
     };
-    
+
     list.forEach((item) => {
         item.addEventListener('click', activeLink)
     });
