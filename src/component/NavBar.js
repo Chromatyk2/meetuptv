5d9f3b7ba02ae745f093441cf59d2c5f06296ee2
pierre schuvey <chromatykretro@gmail.com>
2024-09-18 16:20:27 +0200

    fix

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -13,62 +13,62 @@ function NavBar(props) {
     const [stream, setStream] = useState(null);
     const pseudo = props.cookies.user.data[0].login;
     const [expanded, setExpanded] = useState(false);
-    return (
+    const list = document.querySelectorAll(".list");
 
-        <Navbar expanded={expanded} bg="light" expand="lg">
-            <Container fluid>
-                <Navbar.Brand><a href={"https://twitch.tv/chromatyk"}><img src={"/logo.png"} /></a></Navbar.Brand>
-                <Navbar.Toggle
-                    aria-controls="navbarScroll"
-                    onClick={() => setExpanded(!expanded)}
-                />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav className="me-auto my-2 my-lg-0" navbarScroll>
-                        <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/">Accueil</Link>
-                        <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/profil">Profil</Link>
-                        <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/pokedex">Pokedex</Link>
-                        <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/allProfils">Classement</Link>
-                        <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/pedandex">Pedandex</Link>
-                        {pseudo == "chromatyk" &&
-                            <>
-                                <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/tcg/cartes">Mes cartes</Link>
-                                <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/tcg/boosters">Mes boosters</Link>
-                                <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/tcg/boutique">Boutique TCG</Link>
-                                <Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/49Vs5sWVS2e7qs">Générateur</Link>
-                                {/*  <Link className="navLink" to="/pokedex">Pokedex</Link>*/}
-                                {/*  <Link className="navLink" to="/leaderboard">Classement</Link>*/}
-                                {/*  <Link className="navLink myTradesLink" to="/myTrades">Mes Echanges {count > 0 && <span className="myCountProposition">{count}</span>}</Link>*/}
-                                {/*  <Link className="navLink" to="/tradePlace">Place aux echanges</Link>*/}
-                                {/*<Link className="navLink" to="/aNu5YwZ5X75m5j">Note</Link>*/}
-                                {/*  <Link className="navLink" to="/29ct92B3ZrvxGS">NostalPick</Link>*/}
-                                {/*<Link onClick={() => setExpanded(false)} className="navLink linkFromNav" to="/tcg/liste">Liste</Link>*/}
-                            </>
-                        }
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-        // <Navbar expand="lg">
-        //   <Container>
-        //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        //     <Navbar.Collapse id="basic-navbar-nav">
-        //       <Nav className="me-auto">
-        //         <Link className="navLink" to="/">Accueil</Link>
-        //           <Link className="navLink" to="/29ct92B3ZrvxGT">TCG</Link>
-        //         {pseudo == "chromatyk" &&
-        //           <>
-        //               <Link className="navLink" to="/pokedex">Pokedex</Link>
-        //               <Link className="navLink" to="/leaderboard">Classement</Link>
-        //               <Link className="navLink myTradesLink" to="/myTrades">Mes Echanges {count > 0 && <span className="myCountProposition">{count}</span>}</Link>
-        //               <Link className="navLink" to="/tradePlace">Place aux echanges</Link>
-        //             <Link className="navLink" to="/aNu5YwZ5X75m5j">Note</Link>
-        //             <Link className="navLink" to="/29ct92B3ZrvxGS">NostalPick</Link>
-        //           </>
-        //         }
-        //       </Nav>
-        //     </Navbar.Collapse>
-        //   </Container>
-        // </Navbar>
+    function activeLink() {
+        list.forEach((item) => item.classList.remove('active'));
+        this.classList.add('active');
+    };
+    
+    list.forEach((item) => {
+        item.addEventListener('click', activeLink)
+    });
+    return (
+        <div className="navbar">
+            <ul>
+                <li className="list active">
+                    <a href={"#"}>
+                    <span className="icon">
+                        <ion-icon name="home-outline"></ion-icon>
+                    </span>
+                        <span className="text">Home</span>
+                    </a>
+                </li>
+                <li className="list">
+                    <a href={"#"}>
+                    <span className="icon">
+                        <ion-icon name="person-outline"></ion-icon>
+                    </span>
+                        <span className="text">Profile</span>
+                    </a>
+                </li>
+                <li className="list">
+                    <a href={"#"}>
+                    <span className="icon">
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                    </span>
+                        <span className="text">Message</span>
+                    </a>
+                </li>
+                <li className="list">
+                    <a href={"#"}>
+                    <span className="icon">
+                        <ion-icon name="camera-outline"></ion-icon>
+                    </span>
+                        <span className="text">Photos</span>
+                    </a>
+                </li>
+                <li className="list">
+                    <a href={"#"}>
+                    <span className="icon">
+                        <ion-icon name="settings-outline"></ion-icon>
+                    </span>
+                        <span className="text">Settings</span>
+                    </a>
+                </li>
+                <div className="bubble"></div>
+            </ul>
+        </div>
     );
 }
 
