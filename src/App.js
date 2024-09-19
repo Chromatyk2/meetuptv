73996eabcb6a7bd01b90541dd2b6791148d56844
pierre schuvey <chromatykretro@gmail.com>
2024-09-19 10:48:32 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from "./component/NavBar";
+import Player from "./component/Player";
 
 function App() {
   return (
       <>
           <div className="App">
-              <Suspense fallback={
-                  <div className="App-loader">
-                      <img src={logo} className="App-logo-loader" alt="logo"/>
-                  </div>
-              }>
                   <NavBar />
-                  <PlayerComponent />
-              </Suspense>
+                  <Player />
           </div>
       </>
   );
