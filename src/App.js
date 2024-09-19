45702ad737b17ac38ff5fcc853b38eb501457b55
pierre schuvey <chromatykretro@gmail.com>
2024-09-19 10:46:03 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import React, { Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const NavBar = React.lazy(() => import('./component/NavBar'));
-const PlayerComponent = React.lazy(() => import('./component/Player'));
-const NavComponent = React.lazy(() => import('./component/Nav'));
-const Footer = React.lazy(() => import('./component/footer'));
-
 function App() {
   return (
       <>
