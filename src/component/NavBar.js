2a5c906bf5be300ef763633c052392ab862d77c6
pierre schuvey <chromatykretro@gmail.com>
2024-09-19 10:54:05 +0200

    fix

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,17 +1,11 @@
-import React,{useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom'
+import React,{useState} from 'react';
 import Container from 'react-bootstrap/Container';
-import Axios from 'axios'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import {BrowserRouter, Link} from "react-router-dom";
-import env from "react-dotenv";
+import {Link} from "react-router-dom";
 import logo from '../logo.svg';
 
 function NavBar(props) {
-    const [count, setCount] = useState(0);
-    const [stream, setStream] = useState(null);
     const pseudo = props.cookies.user.data[0].login;
     const [expanded, setExpanded] = useState(false);
     return (
@@ -42,27 +36,6 @@ function NavBar(props) {
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-        // <Navbar expand="lg">
-        //   <Container>
-        //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        //     <Navbar.Collapse id="basic-navbar-nav">
-        //       <Nav className="me-auto">
-        //         <Link className="navLink" to="/">Accueil</Link>
-        //           <Link className="navLink" to="/29ct92B3ZrvxGT">TCG</Link>
-        //         {pseudo == "chromatyk" &&
-        //           <>
-        //               <Link className="navLink" to="/pokedex">Pokedex</Link>
-        //               <Link className="navLink" to="/leaderboard">Classement</Link>
-        //               <Link className="navLink myTradesLink" to="/myTrades">Mes Echanges {count > 0 && <span className="myCountProposition">{count}</span>}</Link>
-        //               <Link className="navLink" to="/tradePlace">Place aux echanges</Link>
-        //             <Link className="navLink" to="/aNu5YwZ5X75m5j">Note</Link>
-        //             <Link className="navLink" to="/29ct92B3ZrvxGS">NostalPick</Link>
-        //           </>
-        //         }
-        //       </Nav>
-        //     </Navbar.Collapse>
-        //   </Container>
-        // </Navbar>
     );
 }
 
